c4f29cf2060124e93dd81a9d79a1d39a2abbc245
reachAbhirup <reachme.abhirup@gmail.com>
2026-02-08 18:47:19 +0530

    feat: update shipping calculator logic

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -29,5 +29,65 @@
  * @returns {number} Shipping cost, 0 for free shipping, or -1 for invalid input
  */
 export function calculateShipping(weight, country, orderTotal) {
-  // Your code here
+  
+  let shippingCost;
+
+  // edge cases
+  if( weight <= 0 || orderTotal < 0){
+    return -1;
+  }
+
+  // US Shipping
+  if( country === "US"){
+
+    if( orderTotal > 50){    // FREE shipping category
+      return 0;
+    } else{
+      switch(true){           //NON FREE shipping category
+        case (weight <= 1):
+          shippingCost = 5;
+          break;
+        case (weight > 1 && weight <= 5):
+          shippingCost = 10;
+          break;
+        case (weight > 5 && orderTotal < 50):
+          shippingCost = 15;
+          break;
+      }
+    }
+
+  }
+
+  // International Shipping
+  if( country !== "US"){
+    
+    if( orderTotal >100){     // FREE shipping category
+      return 0;
+    }else {
+      switch(true){           //NON FREE shipping category
+          case (weight <= 1):
+            shippingCost = 15;
+            break;
+          case (weight > 1 && weight <= 5):
+            shippingCost = 25;
+            break;
+          case (weight > 5  && orderTotal < 100):
+            shippingCost = 40;
+            break;
+        }
+    }
+  }
+
+  return shippingCost;
 }
+
+// // Unit Tests
+// console.log(calculateShipping(1,"US",51));
+// console.log(calculateShipping(1,"US",49));
+// console.log(calculateShipping(4,"US",49));
+// console.log(calculateShipping(5,"US",50));
+// console.log(calculateShipping(6,"US",34));
+// console.log(calculateShipping(0.5,"IN",34));
+// console.log(calculateShipping(5,"UK",100));
+// console.log(calculateShipping(6,"FR",160));
+// console.log(calculateShipping(5.1,"IN",14));
